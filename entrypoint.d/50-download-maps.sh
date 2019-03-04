4bbe1cb7b9e5d7f8881fcb7bea21e3184ccf1f30
Blackskyliner <Blackskyliner@users.noreply.github.com>
2019-03-04 22:15:11 +0100

    Chown Mapfiles

--- entrypoint.d/50-download-maps.sh
+++ entrypoint.d/50-download-maps.sh
@@ -7,6 +7,7 @@ if [[ -n $ET_MAPS ]]; then
     for MAP in $ET_MAPS; do
         if [[ ! -e "/opt/application/etmain/$MAP.pk3" ]]; then
             wget -q -O "/opt/application/etmain/$MAP.pk3" "$ET_POOL/$MAP.pk3"
+	    chown ${APPLICATION_UID:-1000}:${APPLICATION_GID:-1000} "$ET_POOL/$MAP.pk3"
         fi
     done
 fi
