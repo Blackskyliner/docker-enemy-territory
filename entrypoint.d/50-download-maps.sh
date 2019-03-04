f44356001f7e55f6ca02ae1016277be2bdae1f01
Blackskyliner <Blackskyliner@users.noreply.github.com>
2019-03-04 22:41:20 +0100

    Fix: Unsilence Map download

--- entrypoint.d/50-download-maps.sh
+++ entrypoint.d/50-download-maps.sh
@@ -6,11 +6,7 @@ ET_MAPS=${ET_MAPS:-""}
 if [[ -n $ET_MAPS ]]; then
     for MAP in $ET_MAPS; do
         if [[ ! -e "/opt/application/etmain/$MAP.pk3" ]]; then
-            wget -q -O "/opt/application/etmain/$MAP.pk3" "$ET_POOL/$MAP.pk3"
-            if [[ ! $? ]]; then
-                echo "Could not download $ET_POOL/$MAP.pk3"
-                exit 1
-            fi
+            wget -O "/opt/application/etmain/$MAP.pk3" "$ET_POOL/$MAP.pk3"
             chown ${APPLICATION_UID:-1000}:${APPLICATION_GID:-1000} "/opt/application/etmain/$MAP.pk3"
         fi
     done
