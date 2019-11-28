85751f71ba99b92e98352013e690c1ffa10fba19
Blackskyliner <Blackskyliner@users.noreply.github.com>
2019-11-28 21:46:07 +0100

    feat: Add .csv Mapdownload + Use /opt/mappool

--- entrypoint.d/50-download-maps.sh
+++ entrypoint.d/50-download-maps.sh
@@ -3,11 +3,25 @@
 ET_POOL=${ET_POOL:-"http://mappool.sexygaming.de/etmain"}
 ET_MAPS=${ET_MAPS:-""}
 
+# Maps by environment
 if [[ -n $ET_MAPS ]]; then
     for MAP in $ET_MAPS; do
-        if [[ ! -e "/opt/application/etmain/$MAP.pk3" ]]; then
-            wget -O "/opt/application/etmain/$MAP.pk3" "$ET_POOL/$MAP.pk3"
-            chown ${APPLICATION_UID:-1000}:${APPLICATION_GID:-1000} "/opt/application/etmain/$MAP.pk3"
+        if [[ ! -e "/opt/mappool/$MAP" ]]; then
+            run-as-user wget -q -O "/opt/mappool/$MAP" "$ET_POOL/$MAP"
         fi
     done
 fi
+
+# Maps by .csv file
+if [[ -e /opt/mappool/maps.csv ]]; then
+    for MAP in $(cat /opt/mappool/maps.csv); do
+        if [[ ! -e "/opt/mappool/$MAP" ]]; then
+            run-as-user wget -q -O "/opt/mappool/$MAP" "$ET_POOL/$MAP"
+        fi
+    done
+fi
+
+# Copy cached maps into gamedir
+# Symlinks are broken so we have to do it this way
+# Hardlinks are not possible because docker (mount) volumes.
+run-as-user find /opt/mappool -name '*.pk3' -exec cp -n -v {} /opt/application/etmain \;
